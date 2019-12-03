a4facbb0b19fb70f1ccff3f429c2de9f3392852e
maricela <laram@mail.gvsu.edu>
2019-12-02 20:35:13 -0500

    add ApplicationDBContext.cs

---
+++ Rocinante/Data/Migrations/20191203011541_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rocinante.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Job",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    Snippet = table.Column<string>(nullable: true),
+                    Link = table.Column<string>(nullable: true),
+                    Company = table.Column<string>(nullable: true),
+                    Updated = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Job", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Job");
+        }
+    }
+}
